1ef85d2caf7f35843c8f3af4859db74fc4b16300
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-01-27 04:25:12 +0800

    Update cifar_training.py

--- examples/vision/cifar_training.py
+++ examples/vision/cifar_training.py
@@ -114,10 +114,11 @@ def Train(model, t, loader, eps_scheduler, norm, train, opt, bound_type, method=
         start = time.time()
         eps_scheduler.step_batch()
         eps = eps_scheduler.get_eps()
+        eps = 0.007
         # For small eps just use natural training, no need to compute LiRPA bounds
         batch_method = method
-        if eps < 1e-50:
-            batch_method = "natural"
+        # if eps < 1e-50:
+        #     batch_method = "natural"
         if train:
             opt.zero_grad()
         # bound input for Linf norm used only
