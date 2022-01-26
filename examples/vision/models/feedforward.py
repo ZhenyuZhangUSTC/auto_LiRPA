ce75d9d09c6fd64c577c9f32ef2189fb81091b2c
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-01-27 04:33:21 +0800

    Update feedforward.py

--- examples/vision/models/feedforward.py
+++ examples/vision/models/feedforward.py
@@ -196,7 +196,7 @@ def cifar_cnn_b(in_ch=3, in_dim=32):
     # cifar_cnn_b
     return nn.Sequential(
         # nn.ZeroPad2d((1,2,1,2)),
-        nn.Conv2d(3, 32, (5,5), stride=2, padding=(1,2,1,2)),
+        nn.Conv2d(3, 32, (5,5), stride=2, padding=(1,2)),
         nn.ReLU(),
         nn.Conv2d(32, 128, (4,4), stride=2, padding=1),
         nn.ReLU(),
